54d3bf6e44b1b9b8594a6d52e915a6cd4b4841d9
Lucas França <lucas.c.franca@gmail.com>
2026-03-23 19:41:49 -0300

    Setting initial environment

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
 from fastapi import FastAPI
-import requests
 
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/city")
+def city_function():
+    return {"temporary return"}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
-
-@app.get("/teste")
-def read_root():
-    resposta = requests.get("https://countriesnow.space/api/v0.1/countries/states")
-    return resposta.text
+@app.get("/country")
+def country_function():
+    return {"temporary return"}
