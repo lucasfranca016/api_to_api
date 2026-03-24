55b39f4191432b9893eb413337eaa104a4d622d4
Hugo Carvalho <hugoncarv@gmail.com>
2026-03-24 00:06:04 -0300

    feat: adiciona endpoint para filtrar cidades de um país

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI
-
+import requests
 
 app = FastAPI()
 
 
-@app.get("/city")
-def city_function():
-    return {"temporary return"}
+@app.get("/{country}/cities")
+def city_function(country: str):
+    cities_url = "https://countriesnow.space/api/v0.1/countries/cities"
+
+    response_cities = requests.post(cities_url, json={
+        "country": country
+    })
+    return response_cities.json()
+
 
 @app.get("/country")
 def country_function():
