ae5562b71a7201f9b191cae6a7326a6a652a9b7b
Hugo Carvalho <hugoncarv@gmail.com>
2026-03-25 16:41:15 -0300

    feat: adiciona mais de uma requisição para o endpoint "/{country}/cities"
    - adiciona a requisição response_capital, que retorna a capital do país
    - adiciona o número de cidades contabilizadas neste mesmo endpoint
    - melhora a lógica para filtrar os dados do JSON

--- main.py
+++ main.py
@@ -3,16 +3,35 @@ import requests
 
 app = FastAPI()
 
-
 @app.get("/{country}/cities")
 def city_function(country: str):
     cities_url = "https://countriesnow.space/api/v0.1/countries/cities"
-
+    capital_url = "https://countriesnow.space/api/v0.1/countries/capital"
+    
     response_cities = requests.post(cities_url, json={
         "country": country
     })
-    return response_cities.json()
-
+    
+    response_capital = requests.post(capital_url, json={
+        "country": country
+    })
+    
+    info_cities = response_cities.json()
+    
+    info_capital = response_capital.json()
+    
+    cities = info_cities["data"] # Seleciona as cidades
+    
+    capital = info_capital["data"]["capital"] # Seleciona a capital
+    
+    return {
+        "error": info_cities["error"],
+        "msg": info_cities["msg"],
+        "data": {
+            "total_cities" : len(cities),
+            "capital" : capital,
+            "cities" : cities
+    }}
 
 @app.get("/country")
 def country_function():
