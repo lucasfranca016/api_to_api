7562feb221fed9483049a4af82394e4295305f21
Hugo Carvalho <hugoncarv@gmail.com>
2026-04-01 17:52:36 -0300

    feat: add a more efficient cache system with redis on city_functions

--- custom_libs/city_functions.py
+++ custom_libs/city_functions.py
@@ -1,51 +1,59 @@
 import requests
+import redis
+import json
 
-city_cache = {}
+r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
-def get_city_info(country: str):  
+def get_city_info(country: str):
     country = country.lower()
+    cache_key = f"country:{country}"
+
+    # Tenta pegar do cache
+    cached = r.get(cache_key)
+
+    if cached:
+        print("Cache HIT")
+        return json.loads(cached)
+
+    print("Cache MISS")
     
-    if country not in city_cache:
-        cities_url = "https://countriesnow.space/api/v0.1/countries/cities"
-        capital_url = "https://countriesnow.space/api/v0.1/countries/capital"
+    cities_url = "https://countriesnow.space/api/v0.1/countries/cities"
+    capital_url = "https://countriesnow.space/api/v0.1/countries/capital"
+
+    response_cities = requests.post(cities_url, json={
+        "country": country
+    })
     
-        response_cities = requests.post(cities_url, json={
-            "country": country
-        })
+    response_capital = requests.post(capital_url, json={
+        "country": country
+    })
+    
+    info_cities = response_cities.json()
+    
+    info_capital = response_capital.json()
+    
+    try:
+        cities = info_cities["data"] # Seleciona as cidades no JSON
+    
+        capital = info_capital["data"]["capital"] # Seleciona a capital no JSON
         
-        response_capital = requests.post(capital_url, json={
-            "country": country
-        })
+        city_data = {
+            "error": info_cities["error"],
+            "msg": info_cities["msg"],
+            "data": {
+                "total_cities" : len(cities),
+                "capital" : capital,
+                "cities" : cities
+        }}
         
-        info_cities = response_cities.json()
-        
-        info_capital = response_capital.json()
-        
-        try:
-            cities = info_cities["data"] # Seleciona as cidades no JSON
-        
-            capital = info_capital["data"]["capital"] # Seleciona a capital no JSON
-            
-            city_data = {
-                "error": info_cities["error"],
-                "msg": info_cities["msg"],
-                "data": {
-                    "total_cities" : len(cities),
-                    "capital" : capital,
-                    "cities" : cities
-            }}
-            
-            city_cache[country] = city_data
-            
-        except KeyError:
-            print('O país não foi encontrado')
-        
-            city_data = {
-                "error": True,
-                "msg": "country not found",
-            }
-        
-        return city_data
-        
-    else:
-        return city_cache[country]
+    except KeyError:
+        print('O país não foi encontrado')
+    
+        city_data = {
+            "error": True,
+            "msg": "country not found",
+        }
+
+    r.set(cache_key, json.dumps(city_data), ex=300)
+    
+    return city_data
