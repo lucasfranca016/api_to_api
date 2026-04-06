18ad664becbdf7d3d41071e9f87ab4a3607bca13
Hugo Carvalho <hugoncarv@gmail.com>
2026-04-05 22:29:36 -0300

    feat: add error handling in cities endpoint
    returns JSON error response on connection failure and logs error

--- custom_libs/city_functions.py
+++ custom_libs/city_functions.py
@@ -1,4 +1,5 @@
 import requests
+from requests import RequestException
 from custom_libs.cache_functions import use_stored_cache
 from custom_libs.cache_functions import store_cache
 
@@ -13,41 +14,50 @@ def get_city_info(country: str):
     cities_url = "https://countriesnow.space/api/v0.1/countries/cities"
     capital_url = "https://countriesnow.space/api/v0.1/countries/capital"
 
-    response_cities = requests.post(cities_url, json={
-        "country": country
-    })
-    
-    response_capital = requests.post(capital_url, json={
-        "country": country
-    })
-    
-    info_cities = response_cities.json() # Transforms into a dict
-
-    info_capital = response_capital.json() # Transforms into a dict
-    
     try:
-        cities = info_cities["data"] # Select the cities from the converted JSON
-    
-        capital = info_capital["data"]["capital"] # Select the capital from the converted JSON
+        response_cities = requests.post(cities_url, json={
+            "country": country
+        })
         
-        city_data = {
-            "error": info_cities["error"],
-            "msg": info_cities["msg"],
-            "data": {
-                "total_cities" : len(cities),
-                "capital" : capital,
-                "cities" : cities
-        }}
+        response_capital = requests.post(capital_url, json={
+            "country": country
+        })
         
-    except KeyError:
-        print('O país não foi encontrado')
-    
-        city_data = {
-            "error": True,
-            "msg": "country not found",
-        }
+        info_cities = response_cities.json() # Transforms into a dict
 
-    # Store the data in Redis using cache key. dumps() converts the dict into a string, since Redis stores data as strings
+        info_capital = response_capital.json() # Transforms into a dict
+        
+        try:
+            cities = info_cities["data"] # Select the cities from the converted JSON
+        
+            capital = info_capital["data"]["capital"] # Select the capital from the converted JSON
+    
+            city_data = {
+                "error": info_cities["error"],
+                "msg": info_cities["msg"],
+                "data": {
+                    "total_cities" : len(cities),
+                    "capital" : capital,
+                    "cities" : cities
+            }}
+            
+        except KeyError:
+            print('O país não foi encontrado')
+        
+            city_data = {
+                "error": True,
+                "msg": "country not found",
+            }
+
+    except RequestException:
+        print('Não foi possível fazer a requisição.')
+        
+        city_data = {
+                "error": True,
+                "msg": "failed to connect",
+            }
+        
+        # Store the data in Redis using cache key. dumps() converts the dict into a string, since Redis stores data as strings
     store_cache(cache_key, city_data)
     
     return city_data
