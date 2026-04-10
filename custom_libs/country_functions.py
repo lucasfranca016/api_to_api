991876d63584158a4bc11ac7d85d9de971e21511
Hugo Carvalho <hugoncarv@gmail.com>
2026-04-10 17:23:05 -0300

    feat: add retry function on country endpoint

--- custom_libs/country_functions.py
+++ custom_libs/country_functions.py
@@ -3,6 +3,7 @@ from custom_libs.cache_functions import use_stored_cache
 from custom_libs.cache_functions import store_cache
 from tenacity import retry, stop_after_attempt, wait_exponential
 from requests import RequestException
+from custom_libs.retry_functions import should_retry
 
 @retry(stop=stop_after_attempt(5), wait=wait_exponential(multiplier=1, min=1, max=10))
 def get_country_info(country_name):
@@ -26,11 +27,18 @@ def get_country_info(country_name):
         )
         
         response_population.raise_for_status()
+        
+        should_retry(response_population)
 
         # TODO: Make the remaining requests and combine the results
         
     except RequestException as error:
-        print(f'Request error: {error}')
+        if error.response is not None:
+            print(f'HTTP error: {error.response.status_code}')
+            return {
+            "error": True,
+            "msg": f"HTTP error {error.response.status_code}"
+            }
         raise
         
     store_cache(cache_key, response_population.json())
